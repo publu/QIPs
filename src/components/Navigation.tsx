6506b6de672546398a3b6d6f4ded9f01c838ba02
Binod2266 <binod_k@virtual-gravity.com>
2024-01-10 19:31:56 +0545

    Feat: Added template page and it's routing

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,36 +1,38 @@
-import React from 'react';
+import React from "react";
 
 // Navigation component
 const Navigation = () => {
-    return (
-        <nav className="navbar bg-gray-200 w-full fixed top-0 p-4 flex justify-center items-center z-50">
-            <div className="flex justify-center w-[50%]">
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-6 w-6 text-skin-link"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                >
-                    <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth={2}
-                        d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"
-                    />
-                </svg>
-                <a href="/" className="ml-2 text-xl font-bold">Proposals</a>
-            </div>
-            <div className="flex justify-center w-[50%]">
-                <a
-                    href="/all-proposals"
-                    className="text-skin-link cursor-pointer"
-                >
-                    All Proposals
-                </a>
-            </div>
-        </nav>
-    );
+  return (
+    <nav className="navbar bg-gray-200 w-full fixed top-0 p-4 flex justify-center items-center z-50">
+      <div className="flex justify-center w-[50%]">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6 text-skin-link"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"
+          />
+        </svg>
+        <a href="/" className="ml-2 text-xl font-bold">
+          Proposals
+        </a>
+      </div>
+      <div className="flex gap-5 justify-center w-[50%]">
+        <a href="/all-proposals" className="text-skin-link cursor-pointer">
+          All Proposals
+        </a>
+        <a href="/all-templates" className="text-skin-link cursor-pointer">
+          All Templates
+        </a>
+      </div>
+    </nav>
+  );
 };
 
-export default Navigation
+export default Navigation;
---
+++ src/pages/all-templates.tsx
@@ -0,0 +1,89 @@
+import React from "react";
+import { graphql } from "gatsby";
+import Navigation from "../components/Navigation";
+import ProposalListItem from "../components/ProposalListItem";
+import { sortBy, filter, flow } from "lodash/fp";
+
+//@ts-ignore
+import statuses from "../../ps/statuses";
+import Layout from "../layout";
+interface Props {
+  data: any;
+}
+
+// All Proposals component
+const AllTemplates: React.FC<Props> = ({ data: { allMarkdownRemark } }) => {
+  const { group } = allMarkdownRemark;
+  const columns = flow(
+    filter(({ fieldValue }) => statuses.indexOf(fieldValue) > -1),
+    sortBy(({ fieldValue }) => statuses.indexOf(fieldValue))
+  )(group) as any;
+
+  return (
+    <Layout>
+      <div className="content mt-30 overflow-y-auto h-screen flex justify-center items-start">
+        <div
+          id="content-center"
+          className="relative w-full pl-0 lg:w-3/4 lg:pl-5 mt-20"
+        >
+          <div className="mb-3 space-y-3 px-3">
+            <div className="mb-16"></div>
+
+            {columns.map((column: any) => {
+              const proposals = sortBy("frontmatter.proposalId")(column.nodes);
+              return (
+                <div
+                  key={column.fieldValue}
+                  className="proposal-list-container"
+                >
+                  <div className="shadow-s p-5">
+                    <h3 className="text-2xl font-semibold mb-3">
+                      {column.fieldValue}
+                    </h3>
+                  </div>
+
+                  <ProposalListItem proposals={proposals} />
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export const query = graphql`
+  query {
+    allMarkdownRemark(
+      filter: {
+        fileAbsolutePath: { regex: "/QIP/" }
+        frontmatter: { qip: { ne: null } }
+      }
+    ) {
+      group(field: { frontmatter: { status: SELECT } }) {
+        fieldValue
+        totalCount
+        nodes {
+          id
+          frontmatter {
+            qip
+            title
+            shortDescription
+            author
+            network
+            type
+            proposal
+            implementor
+            release
+            created
+            updated
+            status
+          }
+        }
+      }
+    }
+  }
+`;
+
+export default AllTemplates;
