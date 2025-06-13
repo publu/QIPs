c9633a95901eecc296880a5fa505ce1e20da16f2
Royalaid <2439803+royalaid@users.noreply.github.com>
2025-06-13 04:10:31 -0700

    Refactor Footer component layout for improved responsiveness and readability
    - Adjusted layout to use `w-fit` for the title section.
    - Simplified structure by removing unnecessary divs and improving padding.
    - Enhanced text wrapping for better presentation of QIP description.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,25 +2,20 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <footer className="bg-gray-200 mt-auto">
-            <div className="wrapper px-3 py-7 flex flex-col md:flex-row md:items-center justify-between">
-                <div className="w-full md:w-1/6">
-                    <h2 className="text-2xl font-bold mb-4">QIPs</h2>
-                </div>
+      <footer className="bg-gray-200 mt-auto">
+        <div className="wrapper px-3 py-7 flex flex-col md:flex-row md:items-center">
+          <div className="w-fit md:w-1/6">
+            <h2 className="text-2xl font-bold mb-4">QIPs</h2>
+          </div>
 
-                <div className="w-full md:w-1/2 md:mx-10">
-                    <p className="mb-4">
-                        Qi Dao Improvement Proposals (QIPs) describe standards
-                        for the Qi Dao platform, including core protocol
-                        specifications, client APIs, and contract standards.
-                    </p>
-                </div>
-
-                <div className="w-full md:w-1/3 text-lg font-semibold flex flex-col items-center md:items-start">
-                    <p className="mb-4">Contact</p>
-                </div>
-            </div>
-        </footer>
+          <div className="w-full px-10 lg:px-6 md:px-0">
+            <p className="w-full  mb-4">
+              Qi Dao Improvement Proposals (QIPs) describe standards for the Qi Dao platform, including core protocol specifications, client
+              APIs, and contract standards.
+            </p>
+          </div>
+        </div>
+      </footer>
     );
 };
 
